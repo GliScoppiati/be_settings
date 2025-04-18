ed85b8c57b35f61f8cd8c4e61eff190d69284873
smontuor <smontuor@student.42firenze.it>
2025-04-18 17:44:47 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone git@github.com:GliScoppiati/be_settings.git main
+
+git clone git@github.com:GliScoppiati/be_Gateway.git main/Gateway
+
+git clone git@github.com:GliScoppiati/be_AuthService.git main/AuthService
+
+git clone git@github.com:GliScoppiati/be_UserProfileService.git main/UserProfileService
