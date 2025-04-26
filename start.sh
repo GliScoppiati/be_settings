1eebc7faebaabba2b346e2442615f16028ff462b
smontuor <smontuor@student.42firenze.it>
2025-04-26 16:30:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,5 @@ git clone git@github.com:GliScoppiati/be_Gateway.git main/Gateway
 git clone git@github.com:GliScoppiati/be_AuthService.git main/AuthService
 
 git clone git@github.com:GliScoppiati/be_UserProfileService.git main/UserProfileService
+
+git clone git@github.com:GliScoppiati/be_CocktailImportService.git main/CocktailImportService
