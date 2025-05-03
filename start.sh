53da86b150d5acf97bf8aaec8043048fbc468d68
Ste_home <smontuor@student.42firenze.it>
2025-05-03 05:24:30 +0200

    update

--- start.sh
+++ start.sh
@@ -9,3 +9,5 @@ git clone git@github.com:GliScoppiati/be_AuthService.git main/AuthService
 git clone git@github.com:GliScoppiati/be_UserProfileService.git main/UserProfileService
 
 git clone git@github.com:GliScoppiati/be_CocktailImportService.git main/CocktailImportService
+
+git clone git@github.com:GliScoppiati/be_CocktailSubmissionService.git main/CocktailSubmissionService
