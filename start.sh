4747b438fd97c1cd73199a4bd4ae75735899829f
smontuor <smontuor@student.42firenze.it>
2025-05-07 02:37:23 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-git clone git@github.com:GliScoppiati/be_settings.git main
+git clone git@github.com:GliScoppiati/be_Gateway.git Gateway
 
-git clone git@github.com:GliScoppiati/be_Gateway.git main/Gateway
+git clone git@github.com:GliScoppiati/be_AuthService.git AuthService
 
-git clone git@github.com:GliScoppiati/be_AuthService.git main/AuthService
+git clone git@github.com:GliScoppiati/be_UserProfileService.git UserProfileService
 
-git clone git@github.com:GliScoppiati/be_UserProfileService.git main/UserProfileService
+git clone git@github.com:GliScoppiati/be_CocktailImportService.git CocktailImportService
 
-git clone git@github.com:GliScoppiati/be_CocktailImportService.git main/CocktailImportService
+git clone git@github.com:GliScoppiati/be_CocktailSubmissionService.git CocktailSubmissionService
 
-git clone git@github.com:GliScoppiati/be_CocktailSubmissionService.git main/CocktailSubmissionService
+git clone git@github.com:GliScoppiati/be_CocktailService.git CocktailService
